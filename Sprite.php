121c65c572753bb6085c5d80ca19d330eca6d4d7
Shawn C. Grav <ShawnCG@users.noreply.github.com>
2017-04-03 17:08:13 -0400

    Update Sprite.php

--- Sprite.php
+++ Sprite.php
@@ -7,41 +7,71 @@ class Sprite
     private $original_height;
     private $original_width;
 
+    public $fit = false;
+
     private $height = 0;
     private $width = 0;
+    private $scale = 1;
 
     private $image_url;
-    private $tmp_image;
+    private $image_resource;
 
     public function __construct ( $sprite_url )
     {
         $this->image_url = $sprite_url;
-        $this->createTmpFile();
-        $this->getDimensions();
+        $this->createImageResource();
     }
 
     public function __get ( $name )
     {
-        if ($name == 'width' || $name == 'height') {
-        }
+        return $this->{$name};
     }
 
-    public function read ()
+    public function height ( $height = false )
     {
-        $image = fopen($this->image_url);
-        return fread($this->image);
+        $this->height = $height;
     }
 
-    private function createTmpFile ()
+    public function width ( $width = false )
     {
-        $this->tmp_image = tmpfile();
-        $image_contents = file_get_contents($image);
-        fwrite($tmp, $image_contents);
+        $this->width = $width;
     }
 
-    private function getDimensions ()
+    public function scaleHeight ( $height = false )
     {
+        $this->scale = $height / $this->original_height;
+        $this->height = $height;
+        $this->width = $this->original_width * $this->scale;
     }
 
+    public function scaleWidth ( $width = false )
+    {
+        $this->scale = $width / $this->original_width;
+        $this->width = $width;
+        $this->height = $this->original_height * $this->scale;
+    }
+
+    public function getResource ()
+    {
+        return $this->image_resource;
+    }
+
+    private function createImageResource ()
+    {
+        $image_contents = file_get_contents($this->image_url);
+        $res = imagecreatefromstring($image_contents);
+        
+        $this->width = $this->original_width = imagesx($res);
+        $this->height = $this->original_height = imagesy($res);
+
+        $new = imagecreatetruecolor($this->original_width, $this->original_height);
+        imagecolortransparent($new, imagecolorallocatealpha($new, 0, 0, 0, 127));
+        imagealphablending($new, false);
+        imagesavealpha($new, true);
+
+        imagecopyresampled($new, $res, 0, 0, 0, 0, $this->original_width, $this->original_height, $this->original_width, $this->original_height);
+
+        $this->image_resource = $new;
+    }
 
 }
